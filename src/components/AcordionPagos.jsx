decad778e68ed14ab1a9cea258a1a1b42f8ee951
Alan Almaguer <alalmag2@gmail.com>
2024-08-27 18:33:41 -0600

    v1.0

--- src/components/AcordionPagos.jsx
+++ src/components/AcordionPagos.jsx
@@ -36,29 +36,29 @@ export function AccordionVariant() {
         <Page/>
           <div className=' flex flex-col gap-4 text-start text-lg  pl-6 pr-2 text-zinc-300 dark:text-zinc-300'>
             <p className='text-start text-zinc-300 dark:text-zinc-300'>
-              Contamos con diferentes métodos de pago para que elijas el que más te guste:
+              Contamos con diferentes métodos de pago para que elijas el que más te convenga:
             </p>
             <ul className='list-disc'>
               <li>Pago en efectivo</li>
               <li>Pago con tarjeta</li>
               <li>Transferencia</li>
-              <li>Domiciliado de tarjeta</li>
+              <li>Domiciliación de tarjeta</li>
             </ul>
             <p className=''>
-              Para pagar por cualquiera de los métodos anteriores es necesario ingresar al <a href='https://onfiber.trytoku.com/' className='underline font-semibold text-pink-400'>Portal de pago</a> con tu número de usuario y elegir el método deseado.
+              Para pagar por cualquiera de los métodos anteriores es necesario ingresar al <a href='https://onfiber.trytoku.com/' className='underline font-semibold text-pink-400'>Portal de pago</a> con tu número de cliente y elegir el método deseado.
             </p>
             <p className=' font-bold'>
-              Si no conoces tu número de cliente favor de comunicarte con nosotros via whatsapp para que un agente te ayude en el proceso
+              Si no conoces tu número de cliente, favor de comunicarte con nosotros vía WhatsApp para que un agente te ayude en el proceso.
             </p>
             <p className="text-lg">WHATSAPP: <a href="https://wa.link/57q5fq" target='_blank' class="text-pink-400 font-bold">449 327 51 98</a></p>
             <div className='flex flex-col gap-4 text-start text-lg text-zinc-300 dark:text-zinc-300'>
               <p className='text-xl font-semibold text-center'>Pago por Oxxo</p>
               <p>
-                Para pagar por Oxxo es necesario ingresar al portal de Onfiber. Para ingresar, al contratar el servicio, el sistema le hizo llegar un usuario y contraseña vía SMS al número registrado en la contratación. Estos son sus datos para ingresar al portal.
+                Para pagar por Oxxo es necesario ingresar al portal de OnFiber. Al contratar el servicio, recibirás un usuario y contraseña vía SMS al número registrado. Estos datos son necesarios para acceder al portal.
               </p>
               <p className="mt-2"><a href="https://onfiber.mikrowisp.net/cliente/login" target="_blank" class="text-pink-400 underline">PORTAL DE CLIENTES</a></p>
               <p>
-                Si no cuenta con estos datos, puede comunicarse por WhatsApp al siguiente número para que se los proporcione: <a href="https://wa.link/jh5htf" target="_blank" class="text-pink-400 underline">4493275135</a>.
+                Si no cuentas con estos datos, puedes comunicarte por WhatsApp al siguiente número para que te los proporcionen: <a href="https://wa.link/jh5htf" target="_blank" class="text-pink-400 underline">4493275135</a>.
               </p>
               <p className="mt-2">
                 Recomendamos descargar el manual para no tener problemas en el proceso: <a href="/docs/manual_de_pago_oxxo_onfiber.pdf" target="_blank" class="text-pink-400 underline">Descargar manual</a>
@@ -96,11 +96,11 @@ export function AccordionVariant() {
               Los únicos números para soporte, contratación y resolver dudas son los siguientes:
             </p>
             <div className="flex flex-row items-center gap-2">
-              <img src="/phone.svg" alt="Mail" class="h-10"/>
+              <img src="/phone.svg" alt="Teléfono" class="h-10"/>
               <p className="text-lg">LLÁMANOS: <a href="tel:4493880998" class="text-pink-400 font-bold">449 388 09 98</a></p>
             </div>
             <div className="flex flex-row items-center gap-2">
-              <img src="/whatsapp.svg" alt="Mail" class="h-10"/>
+              <img src="/whatsapp.svg" alt="WhatsApp" class="h-10"/>
               <p className="text-lg">WHATSAPP: <a href="https://wa.link/57q5fq" target='_blank' class="text-pink-400 font-bold">449 327 51 98</a></p>
             </div>
             <p className='font-semibold text-red-600'>FAVOR DE NO COMUNICARSE CON SU INSTALADOR O VENDEDOR.</p>
@@ -113,23 +113,23 @@ export function AccordionVariant() {
           <div className='flex items-center'>
             <ChevronRight className='h-4 w-4 text-zinc-950 transition-transform duration-200 group-data-[expanded]:rotate-90 dark:text-zinc-50' />
             <div className='ml-2 text-zinc-950 dark:text-zinc-50 text-2xl' style={{'color':'#FFB300'}}>
-              Cuidados y recomendaciones con el modem
+              Cuidados y recomendaciones con el módem
             </div>
           </div>
         </AccordionTrigger>
         <AccordionContent className='origin-left border-b-2 border-white'>
           <div className='flex flex-col gap-4 text-start text-lg pl-6 pr-2 text-zinc-300 dark:text-zinc-300'>
             <p>
-              El modem es un dispositivo que se utiliza para conectarse a internet. Funciona como un puente entre la señal de internet que llega a tu domicilio y tus dispositivos electrónicos (computadora, teléfono, tablet, etc.).
+              El módem es un dispositivo esencial para conectarse a internet. Actúa como un puente entre la señal de internet que llega a tu domicilio y tus dispositivos electrónicos (computadora, teléfono, tableta, etc.).
             </p>
             <p>
-              El rango de alcance del modem es de aproximadamente 5 metros a la redonda para que sea eficaz.
+              El rango de alcance del módem es de aproximadamente 5 metros a la redonda para asegurar su efectividad.
             </p>
             <p>
-              Si tu modem tiene la característica de conexión de doble banda (2.4 GHz y 5 GHz), se recomienda conectarse a la red de 5 GHz para obtener una conexión más rápida y estable.
+              Si tu módem tiene la capacidad de conexión de doble banda (2.4 GHz y 5 GHz), se recomienda conectarse a la red de 5 GHz para una conexión más rápida y estable.
             </p>
             <p>
-              Es importante mantener el modem limpio y libre de plagas de insectos, ya que pueden afectar su funcionamiento. También se recomienda no manipularlo demasiado, ya que esto podría dañar el cableado interno y afectar su desempeño.
+              Es importante mantener el módem limpio y libre de plagas de insectos, ya que podrían afectar su funcionamiento. Además, se recomienda manipularlo con cuidado para evitar dañar el cableado interno y afectar su rendimiento.
             </p>
           </div>
         </AccordionContent>
